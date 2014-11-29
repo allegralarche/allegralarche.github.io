dbf340e93c6f34a8859d170b837ac0f77012d418
Allegra Larche <allegralarche@gmail.com>
2014-11-29 18:35:42 +0000

    js added

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,3 +1,26 @@
-function navClick() {
-    var navList = document.getElementbyId("navbar");
+function navClick(name) {
+    hideAll();
+    if(name==="about") {
+        $("#about").show();
+    }
+    if(name==="course") {
+        $("#course").show();
+    }
+    if(name==="experience") {
+        $("#experience").show();
+    }
+    if(name==="contact") {
+        $("#contact").show();
+    }
 }
+
+function hideAll() {
+    $("#about").hide();
+    $("#course").hide();
+    $("#experience").hide();
+    $("#contact").hide();
+}
+
+$(document).ready(function() {
+    navClick("about");
+})
